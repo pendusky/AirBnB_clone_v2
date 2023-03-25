0455c027c2d9e539fb6c4bfd90541da831c2a211
pendusky <pendusky@gmail.com>
2023-03-25 18:01:50 +0000

    update

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# script that creates a folder if one doesn't already exist
+
+sudo apt-get -y update
+sudo apt-get -y install nginx
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test
+sudo echo "Best School" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -hR ubuntu:ubuntu /data/
+sudo sed -i "37i\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/\n\t}" /etc/nginx/sites-available/default
+sudo service nginx restart
